0e12513e7d1796d433135bb8c9a0d619238bdcf2
unknown <henryomosh7@gmail.com>
2025-09-25 15:24:53 +0300

    7th commit

--- app/api/validation/route.ts
+++ app/api/validation/route.ts
@@ -3,12 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    return {
-      ResultCode: "0",
-      ResultDesc: "Accepted",
-    };
   } catch (error) {
-    error;
+    return NextResponse.json({ message: "some error occured" });
   }
-  return NextResponse.json({ message: "success" });
+  return NextResponse.json({
+    ResultCode: "0",
+    ResultDesc: "Accepted",
+  });
 }
