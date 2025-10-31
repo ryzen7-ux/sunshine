4022708934945e83dbf16e1e82a78d91be045c10
henryomosh <henryomosh7@gmail.com>
2025-10-31 09:11:28 +0300

    changed add members

--- app/ui/loans/buttons.tsx
+++ app/ui/loans/buttons.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import 
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { deleteLoan } from "@/app/lib/sun-actions";
